4d3c48f0d4955f3ed36726c99c7b7bb2547faf5d
wyang1006 <wyang@ctseinc.com>
2024-08-20 14:33:11 -0400

    Update api.py

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ if button('CLICK'):
 embed_url="https://app.powerbi.com/view?r=eyJrIjoiZTA2YzkyNTItMWFjMS00OTM1LTlmZGQtODdjMmEyMWZjMTc0IiwidCI6ImYyODEwNzRiLTc1MGQtNGM1Zi1iZDQ0LWYzYjg0OTk5NDk3NiIsImMiOjJ9"
 st.markdown(
     f"""
-    <iframe width="100%" height="800" src="{embed_url}" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe width="2000" height="800" src="{embed_url}" frameborder="0" allowFullScreen="true"></iframe>
     """,
     unsafe_allow_html=True
 )
