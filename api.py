9779da9138690ead3b7112cd38291927704ad0a7
wyang1006 <wyang@ctseinc.com>
2024-08-20 14:31:12 -0400

    Update api.py

--- api.py
+++ api.py
@@ -7,4 +7,9 @@ if button('CLICK'):
     st.write('Welcome')
 
 embed_url="https://app.powerbi.com/view?r=eyJrIjoiZTA2YzkyNTItMWFjMS00OTM1LTlmZGQtODdjMmEyMWZjMTc0IiwidCI6ImYyODEwNzRiLTc1MGQtNGM1Zi1iZDQ0LWYzYjg0OTk5NDk3NiIsImMiOjJ9"
-st.components.v1.html(html_code,height=600)
+st.markdown(
+    f"""
+    <iframe width="100%" height="800" src="{embed_url}" frameborder="0" allowFullScreen="true"></iframe>
+    """,
+    unsafe_allow_html=True
+)
