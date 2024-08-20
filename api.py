1f0695ebb29188c07c9bcf7b7e5522912f59705c
wyang1006 <wyang@ctseinc.com>
2024-08-20 14:22:05 -0400

    Update api.py

--- api.py
+++ api.py
@@ -6,4 +6,4 @@ button=st.button
 if button('CLICK'):
     st.write('Welcome')
 
-st.markdown("https://app.powerbi.com/view?r=eyJrIjoiZTA2YzkyNTItMWFjMS00OTM1LTlmZGQtODdjMmEyMWZjMTc0IiwidCI6ImYyODEwNzRiLTc1MGQtNGM1Zi1iZDQ0LWYzYjg0OTk5NDk3NiIsImMiOjJ9",unsafe_allow_html=Ture)
+st.markdown("https://app.powerbi.com/view?r=eyJrIjoiZTA2YzkyNTItMWFjMS00OTM1LTlmZGQtODdjMmEyMWZjMTc0IiwidCI6ImYyODEwNzRiLTc1MGQtNGM1Zi1iZDQ0LWYzYjg0OTk5NDk3NiIsImMiOjJ9",unsafe_allow_html=True)
