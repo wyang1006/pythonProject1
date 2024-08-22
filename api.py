685cee30ca4c487248fb5adb71d3aa2214a6faf9
wyang1006 <wyang@ctseinc.com>
2024-08-21 20:28:58 -0400

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ st.title("This is a test")
 embed_url="https://app.powerbi.com/view?r=eyJrIjoiZTA2YzkyNTItMWFjMS00OTM1LTlmZGQtODdjMmEyMWZjMTc0IiwidCI6ImYyODEwNzRiLTc1MGQtNGM1Zi1iZDQ0LWYzYjg0OTk5NDk3NiIsImMiOjJ9"
 st.markdown(
     f"""
-    <iframe width="600" height="373.5" src="{embed_url}" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe width="1000" height="600" src="{embed_url}" frameborder="0" allowFullScreen="true"></iframe>
     """,
     unsafe_allow_html=True
 )
