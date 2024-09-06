817bdcc1b8e3dae2253895e9495ed21d24fbef08
wyang1006 <wyang@ctseinc.com>
2024-09-06 08:37:14 -0400

    Update api.py

--- api.py
+++ api.py
@@ -49,7 +49,7 @@ if page == "Freight Flow Dashboard":
     st.markdown(
         f"""
         <div style="display: flex; justify-content: center;">
-            <iframe src="{embed_url}" width="1200" height="800" style="border:none;"></iframe>
+            <iframe src="{embed_url}" width="1100" height="720" style="border:none;"></iframe>
         </div>
         """,
         unsafe_allow_html=True
