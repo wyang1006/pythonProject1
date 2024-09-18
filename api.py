cc20cd2ca727cc67e6c4f2872978b1ec6843c4a6
wyang1006 <wyang@ctseinc.com>
2024-09-18 15:44:27 -0400

    Update api.py

--- api.py
+++ api.py
@@ -102,7 +102,7 @@ else:
         run = client.beta.threads.runs.create_and_poll(
             thread_id=thread.id,
             assistant_id=assistant_id,
-            instructions="When performing data analysis and answering questions, please refer only to the uploaded file.All information comes from two seperate years : 2020 and 2050. If a county name is mentioned without being specified as origin or destination, please ask users to specify it as a origin or a destination"
+            instructions="When performing data analysis and answering questions, please refer only to the uploaded file:FAF_Flow.csv. When users asked about commodity types, limit references to the 12 commodity types listed in the uploaded file. All information should pertain to two separate years: 2020 and 2050. If a county name is mentioned without specifying the year or whether it is the origin or destination, please ask the user to clarify the year and whether it pertains to the origin or destination."
         )
 
         # Retrieve the last message from the assistant's response
