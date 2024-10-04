c718ecaa68cf41909c7cde34a9104b3791a4c1f3
wyang1006 <wyang@ctseinc.com>
2024-10-04 17:13:24 -0400

    Update api.py

--- api.py
+++ api.py
@@ -53,10 +53,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 st.markdown ("FAF data includes commodity flows (in tonnage) between origin and destination zones by commodity type for trucks. Compared to the latest FAF5 data, this tool provides estimates of disaggregated freight flows by commodity for all counties in Florida.")
-sb=st.sidebar.title('Navigation')
-page=st.sidebar.selectbox("Choose A Section",["Freight Flow Dashboard","AI Data Analyst"])
-if page == "Freight Flow Dashboard":
-    st.header("Disaggregated Freight Flow Dashboard")
+st.header("Disaggregated Freight Flow Dashboard")
     embed_url = "https://app.powerbi.com/view?r=eyJrIjoiZDE3MTA1MTQtZjVmYy00ZjkxLWFkZjctMGU5YTZhOWFlMzU5IiwidCI6ImYyODEwNzRiLTc1MGQtNGM1Zi1iZDQ0LWYzYjg0OTk5NDk3NiIsImMiOjJ9"
 
 
@@ -69,125 +66,141 @@ if page == "Freight Flow Dashboard":
         unsafe_allow_html=True
     )
 
-else:
-    st.header("FAF Freight Flow AI Data Analyst")
+#sb=st.sidebar.title('Navigation')
+#page=st.sidebar.selectbox("Choose A Section",["Freight Flow Dashboard","AI Data Analyst"])
+#if page == "Freight Flow Dashboard":
+    # st.header("Disaggregated Freight Flow Dashboard")
+    # embed_url = "https://app.powerbi.com/view?r=eyJrIjoiZDE3MTA1MTQtZjVmYy00ZjkxLWFkZjctMGU5YTZhOWFlMzU5IiwidCI6ImYyODEwNzRiLTc1MGQtNGM1Zi1iZDQ0LWYzYjg0OTk5NDk3NiIsImMiOjJ9"
+    #
+    #
+    # st.markdown(
+    #     f"""
+    #     <div style="display: flex; justify-content: center;">
+    #         <iframe src="{embed_url}" width="1200" height="804" style="border:none;"></iframe>
+    #     </div>
+    #     """,
+    #     unsafe_allow_html=True
+    # )
 
-    # Initial message
-    st.markdown("Welcome to the FAF Freight Flow AI Data Analyst! You can ask AI Data Analyst to 1) perform a data analysis, 2) generate a chart, or 3) create a download file for your specific requirements.")
-    st.markdown("The dataset contains the following columns:")
-    st.markdown("""
-    - 'Origin': all Florida counties
-    - 'Destination': all Florida counties
-    - 'Year': 2020 and 2050
-    - 'Commodity': 12 commodity types (Nondurable Manufacturing, Paper, Chemicals, Other Durable Manufacturing, Lumber, Waste, Food, Petroleum, Agricultural Products, Clay and Stone, Miscellaneous Freight Warehousing, Minerals)
-    - 'Tonnage': numerical tons""")
-
-
-    # function to call assistant API
-
-    def chat_manager(conversation_history):
-        # only refer to the last 5 messages
-        recent_history = conversation_history[-5:]
-        # Create a new thread for each conversation
-        thread = client.beta.threads.create()
-
-        # Add all the messages in the conversation history to the thread
-        for message in recent_history:
-            client.beta.threads.messages.create(
-                thread_id=thread.id,
-                role=message["role"],
-                content=message["content"]
-            )
-
-        # Generate a response from the assistant
-        run = client.beta.threads.runs.create_and_poll(
-            thread_id=thread.id,
-            assistant_id=assistant_id,
-            instructions="When performing data analysis and answering questions, please refer your query source only to the uploaded file:FAF_Flow.csv, and do not use your own projection or other sources.The uploaded file contains the data of year 2020 and the projection data of year 2050. When users mention commodity or commodity types, limit references to the 12 commodity types listed in the uploaded file.If a non-floridian county is mentioned in the question, ask users to query only the counties of the Florida.  If a county name is mentioned without specifying the year or whether it is the origin or destination, please ask the user to clarify the year and whether it pertains to the origin or destination.When users ask about year '2050', do not use your own projections, and limit your query only to the uploaed file pertaining to the year 2050. If users say 'Thanks' or similar phrase to express the gratitude, please reply'You are welcome' or 'No problem!'"
-        )
-
-        # Retrieve the last message from the assistant's response
-        messages = client.beta.threads.messages.list(thread_id=thread.id)
-        last_message = messages.data[0]  # Get the last message from the list
-
-        response_text = ""
-        response_image = None
-        response_file = None
-
-        # return text or chart
-        for m in last_message.content:
-            if m.type == "image_file":
-                image_file_id = m.image_file.file_id
-                image_data = client.files.content(image_file_id)
-                image_bytes = image_data.read()
-                response_image = image_bytes
-
-            elif m.text.annotations:
-                file_path = m.text.annotations[0].text
-                file_name = file_path.split("/")[-1]
-                file_id = m.text.annotations[0].file_path.file_id
-
-                file_data = client.files.content(file_id)
-                file_content = file_data.read()
-                response_file={"name":file_name,"content":file_content}
-                response_text = m.text.value.split(":")[0]
-
-            else:
-                response_text = m.text.value
-
-        return response_image, response_text, response_file
-        
-
-
-
-    # Initialize the chat history
-    if 'messages' not in st.session_state:
-        st.session_state.messages = []
-
-    # Display chat history
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-            if message.get("image"):
-                st.image(message["image"])
-            if message.get("file"):
-                st.download_button(label = f"Download {message['file']['name']}",
-                                   data = message['file']['content'],
-                                   file_name= message['file']['name'])
-
-
-    # Text input for user query
-    if prompt := st.chat_input("Start here! Ask a question about the data"):
-        # Add user message to history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-
-        # Display user's input
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Prepare the full conversation history for the API request
-        conversation_history = st.session_state.messages
-
-        response_image, response_text,response_file = chat_manager(conversation_history)
-
-        response_content = {"role": "assistant", "content": response_text}
-        if response_image:
-            response_content["image"] = response_image
-        if response_file:
-            response_content["file"] = response_file
-
-        # Add combined assistant response to history
-        st.session_state.messages.append(response_content)
-
-        # Display assistant response
-        with st.chat_message("assistant"):
-            st.markdown(response_text)
-            if response_image:
-                st.image(response_image)
-            if response_file:
-                st.download_button(label= f"Download {response_file['name']}",
-                                   data = response_file['content'],
-                                   file_name = response_file['name'])
+# else:
+#     st.header("FAF Freight Flow AI Data Analyst")
+#
+#     # Initial message
+#     st.markdown("Welcome to the FAF Freight Flow AI Data Analyst! You can ask AI Data Analyst to 1) perform a data analysis, 2) generate a chart, or 3) create a download file for your specific requirements.")
+#     st.markdown("The dataset contains the following columns:")
+#     st.markdown("""
+#     - 'Origin': all Florida counties
+#     - 'Destination': all Florida counties
+#     - 'Year': 2020 and 2050
+#     - 'Commodity': 12 commodity types (Nondurable Manufacturing, Paper, Chemicals, Other Durable Manufacturing, Lumber, Waste, Food, Petroleum, Agricultural Products, Clay and Stone, Miscellaneous Freight Warehousing, Minerals)
+#     - 'Tonnage': numerical tons""")
+#
+#
+#     # function to call assistant API
+#
+#     def chat_manager(conversation_history):
+#         # only refer to the last 5 messages
+#         recent_history = conversation_history[-5:]
+#         # Create a new thread for each conversation
+#         thread = client.beta.threads.create()
+#
+#         # Add all the messages in the conversation history to the thread
+#         for message in recent_history:
+#             client.beta.threads.messages.create(
+#                 thread_id=thread.id,
+#                 role=message["role"],
+#                 content=message["content"]
+#             )
+#
+#         # Generate a response from the assistant
+#         run = client.beta.threads.runs.create_and_poll(
+#             thread_id=thread.id,
+#             assistant_id=assistant_id,
+#             instructions="When performing data analysis and answering questions, please refer your query source only to the uploaded file:FAF_Flow.csv, and do not use your own projection or other sources.The uploaded file contains the data of year 2020 and the projection data of year 2050. When users mention commodity or commodity types, limit references to the 12 commodity types listed in the uploaded file.If a non-floridian county is mentioned in the question, ask users to query only the counties of the Florida.  If a county name is mentioned without specifying the year or whether it is the origin or destination, please ask the user to clarify the year and whether it pertains to the origin or destination.When users ask about year '2050', do not use your own projections, and limit your query only to the uploaed file pertaining to the year 2050. If users say 'Thanks' or similar phrase to express the gratitude, please reply'You are welcome' or 'No problem!'"
+#         )
+#
+#         # Retrieve the last message from the assistant's response
+#         messages = client.beta.threads.messages.list(thread_id=thread.id)
+#         last_message = messages.data[0]  # Get the last message from the list
+#
+#         response_text = ""
+#         response_image = None
+#         response_file = None
+#
+#         # return text or chart
+#         for m in last_message.content:
+#             if m.type == "image_file":
+#                 image_file_id = m.image_file.file_id
+#                 image_data = client.files.content(image_file_id)
+#                 image_bytes = image_data.read()
+#                 response_image = image_bytes
+#
+#             elif m.text.annotations:
+#                 file_path = m.text.annotations[0].text
+#                 file_name = file_path.split("/")[-1]
+#                 file_id = m.text.annotations[0].file_path.file_id
+#
+#                 file_data = client.files.content(file_id)
+#                 file_content = file_data.read()
+#                 response_file={"name":file_name,"content":file_content}
+#                 response_text = m.text.value.split(":")[0]
+#
+#             else:
+#                 response_text = m.text.value
+#
+#         return response_image, response_text, response_file
+#
+#
+#
+#
+#     # Initialize the chat history
+#     if 'messages' not in st.session_state:
+#         st.session_state.messages = []
+#
+#     # Display chat history
+#     for message in st.session_state.messages:
+#         with st.chat_message(message["role"]):
+#             st.markdown(message["content"])
+#             if message.get("image"):
+#                 st.image(message["image"])
+#             if message.get("file"):
+#                 st.download_button(label = f"Download {message['file']['name']}",
+#                                    data = message['file']['content'],
+#                                    file_name= message['file']['name'])
+#
+#
+#     # Text input for user query
+#     if prompt := st.chat_input("Start here! Ask a question about the data"):
+#         # Add user message to history
+#         st.session_state.messages.append({"role": "user", "content": prompt})
+#
+#         # Display user's input
+#         with st.chat_message("user"):
+#             st.markdown(prompt)
+#
+#         # Prepare the full conversation history for the API request
+#         conversation_history = st.session_state.messages
+#
+#         response_image, response_text,response_file = chat_manager(conversation_history)
+#
+#         response_content = {"role": "assistant", "content": response_text}
+#         if response_image:
+#             response_content["image"] = response_image
+#         if response_file:
+#             response_content["file"] = response_file
+#
+#         # Add combined assistant response to history
+#         st.session_state.messages.append(response_content)
+#
+#         # Display assistant response
+#         with st.chat_message("assistant"):
+#             st.markdown(response_text)
+#             if response_image:
+#                 st.image(response_image)
+#             if response_file:
+#                 st.download_button(label= f"Download {response_file['name']}",
+#                                    data = response_file['content'],
+#                                    file_name = response_file['name'])
                 
 
 # 
