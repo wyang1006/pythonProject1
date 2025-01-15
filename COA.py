54776db8502241da9fc8534a04bac4b76c927686
wyang1006 <wyang@ctseinc.com>
2025-01-15 11:09:24 -0500

    Create COA.py

---
+++ COA.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Miami Transit COA",  # Title of the tab in the browser
+    page_icon="🚌",  # Emoji or path to an image file to use as the favicon
+    layout="wide",  # Can be "centered" or "wide"
+)
+st.markdown('test')
